ea745256121988320c892f6575927b57550ffbbc
13-1-kae <kazakovem@gmail.com>
2016-10-25 23:05:26 +0300

    dsfg

--- 6.py
+++ 6.py
@@ -1,24 +1,24 @@
 dict = dict()
 sl = open('en-ru6.txt')
-slt = open('output.txt', 'w')
+another_dic = open('output.txt', 'w')
 s = sl.readline().rstrip()
 while len(s) > 0:
-    en_trans, ru_trans = list(s.split('\t-\t'))
-    if ',' in ru_trans:
-        for i in ru_trans.split(','):
+    trans, russkiy = list(s.split('\t-\t'))
+    if ',' in russkiy:
+        for i in russkiy.split(','):
             i = i.lstrip()
             if i in dict:
-                dict[i] = dict[i]+', '+en_trans
+                dict[i] = dict[i]+', '+trans
             else:
-                dict[i] = en_trans
+                dict[i] = trans
     else:
-        if ru_trans in dict:
-            dict[ru_trans] = dict[ru_trans]+', '+en_trans
+        if russkiy in dict:
+            dict[russkiy] = dict[russkiy]+', '+trans
         else:
-            dict[ru_trans] = en_trans
+            dict[russkiy] = trans
 
     s = sl.readline().rstrip()
 
 key_sort = sorted(dict.keys())
 for i in key_sort:
-    print('\t-\t'.join((i, dict[i])), file=slt)
+    print('\t-\t'.join((i, dict[i])), file=another_dic)
